8b67c685c61c48930aef24653b57f6307aafd4ee
affableashish <affableashk@gmail.com>
2017-09-20 21:15:57 -0500

    Add project files.

---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Video vid = new Video(){Title = "Game of Thrones"};
+            VideoEncoder vidEnc = new VideoEncoder(); // publisher
+            MailService ms = new MailService(); // subscriber
+            vidEnc.VideoEncoded += ms.LaundaVideoEncoded;
+
+            vidEnc.Encode(vid);
+
+        }
+    }
+
+    class MailService
+    {
+        public void LaundaVideoEncoded(object source, VideoEventArgs e)
+        {
+            Console.WriteLine("Sending an E-mail" +" " + e.Vfd.Title);
+        }
+    }
+}
---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace EventsAndDelegates
+{
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Vfd { get; set; }
+    }
+    public class VideoEncoder
+    {
+        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs e);
+
+        public event VideoEncodedEventHandler VideoEncoded; 
+        
+
+
+
+        public void Encode(Video vid)
+        {
+            Console.WriteLine("Video is Encoding");
+            Thread.Sleep(300);
+            OnVideoEncoded(vid);
+            
+        }
+
+
+        protected virtual void OnVideoEncoded(Video vid)
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, new VideoEventArgs(){Vfd = vid});
+        }
+    }
+}
