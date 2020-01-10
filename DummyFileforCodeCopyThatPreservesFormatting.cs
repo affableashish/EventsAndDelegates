c830f6c0418c8641beb559b0119c3966f17a8c79
affableashish <30603497+affableashish@users.noreply.github.com>
2020-01-10 13:54:12 -0500

    Update DummyFileforCodeCopyThatPreservesFormatting.cs

--- DummyFileforCodeCopyThatPreservesFormatting.cs
+++ DummyFileforCodeCopyThatPreservesFormatting.cs
@@ -37,3 +37,42 @@ public class Example
         Console.ReadKey();
     }
 }
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Base b = new Derived();
+		Derived d = new Derived();
+		b.SomeMethod();
+		d.SomeMethod();
+		
+		b.SomeOtherMethod();
+		d.SomeOtherMethod();
+	}
+}
+public class Base
+{
+  public virtual void SomeMethod()
+  {
+	  Console.WriteLine("Some method in Base");
+  }
+  public virtual void SomeOtherMethod()
+  {
+	  Console.WriteLine("Some Other method in Base");
+  }
+}
+
+public class Derived : Base
+{
+  public override void SomeMethod()
+  {
+  	 Console.WriteLine("Some method in Derived");
+  }
+  public new void SomeOtherMethod()
+  {
+  	 Console.WriteLine("Some Other method in Derived");
+  }
+}
